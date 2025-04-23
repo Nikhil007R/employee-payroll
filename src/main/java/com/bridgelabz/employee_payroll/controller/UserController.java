2dbc0fee45baa91a1428f42d439a457cb2e047e1
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-04-24 04:39:20 +0530

    [Nikhil] Add: made some changes in order to differentiate which api is public and which one is authenticated.

--- src/main/java/com/bridgelabz/employee_payroll/controller/UserController.java
+++ src/main/java/com/bridgelabz/employee_payroll/controller/UserController.java
@@ -6,6 +6,7 @@ import com.bridgelabz.employee_payroll.dto.RegisterDTO;
 import com.bridgelabz.employee_payroll.dto.ResponseDTO;
 import com.bridgelabz.employee_payroll.model.User;
 import com.bridgelabz.employee_payroll.repository.UserRepository;
+import com.bridgelabz.employee_payroll.service.UserService;
 import com.bridgelabz.employee_payroll.utility.JwtUserDetailsService;
 import com.bridgelabz.employee_payroll.utility.JwtUtility;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,6 +26,9 @@ import java.util.List;
 public class UserController {
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
@@ -41,76 +45,58 @@ public class UserController {
 
     @GetMapping("/allUsers")
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userService.getAllEmployees();
     }
 
     @GetMapping("/getUser/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return userService.getUserById(id);
     }
 
     @PutMapping("/updateUser/{id}")
     public ResponseEntity<ResponseDTO> updateUser(@PathVariable Long id, @RequestBody RegisterDTO request) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        user.setFullName(request.getFullName());
-        user.setEmail(request.getEmail());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-        userRepository.save(user);
-        ResponseDTO response = new ResponseDTO("User updated successfully", HttpStatus.OK);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return userService.updateUserById(id, request);
     }
 
     @DeleteMapping("/deleteUser/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-        }
-        userRepository.delete(user);
-        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+        return userService.deleteUserById(id);
     }
 
 
-    @PostMapping("/register")
-    public ResponseEntity<ResponseDTO> registerUser(@RequestBody RegisterDTO request) {
-        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
-            ResponseDTO response = new ResponseDTO("user already exists", HttpStatus.NOT_FOUND);
-            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-        }
+//    @PostMapping("/register")
+//    public ResponseEntity<ResponseDTO> registerUser(@RequestBody RegisterDTO request) {
+//        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+//            ResponseDTO response = new ResponseDTO("user already exists", HttpStatus.NOT_FOUND);
+//            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+//        }
+//
+//        User user = new User();
+//        user.setFullName(request.getFullName());
+//        user.setEmail(request.getEmail());
+//        user.setPassword(passwordEncoder.encode(request.getPassword()));
+//        userRepository.save(user);
+//
+//        ResponseDTO response = new ResponseDTO("User registered successfully", HttpStatus.CREATED);
+//        return new ResponseEntity<>(response, HttpStatus.CREATED);
+//    }
 
-        User user = new User();
-        user.setFullName(request.getFullName());
-        user.setEmail(request.getEmail());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-        userRepository.save(user);
-
-        ResponseDTO response = new ResponseDTO("User registered successfully", HttpStatus.CREATED);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<ResponseDTO> loginUser(@RequestBody LoginDTO request) {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-            ResponseDTO response = new ResponseDTO("Invalid Credentials", HttpStatus.UNAUTHORIZED);
-            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
-        }
-
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
-        final String token = jwtUtility.generateToken(userDetails.getUsername());
-
-        ResponseDTO response = new ResponseDTO(token,HttpStatus.OK);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+//    @PostMapping("/login")
+//    public ResponseEntity<ResponseDTO> loginUser(@RequestBody LoginDTO request) {
+//        try {
+//            authenticationManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+//            );
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            ResponseDTO response = new ResponseDTO("Invalid Credentials", HttpStatus.UNAUTHORIZED);
+//            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+//        }
+//
+//        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+//        final String token = jwtUtility.generateToken(userDetails.getUsername());
+//
+//        ResponseDTO response = new ResponseDTO(token,HttpStatus.OK);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
 }
---
+++ src/main/java/com/bridgelabz/employee_payroll/service/UserService.java
@@ -0,0 +1,113 @@
+package com.bridgelabz.employee_payroll.service;
+
+import com.bridgelabz.employee_payroll.dto.LoginDTO;
+import com.bridgelabz.employee_payroll.dto.RegisterDTO;
+import com.bridgelabz.employee_payroll.dto.ResponseDTO;
+import com.bridgelabz.employee_payroll.model.Employee;
+import com.bridgelabz.employee_payroll.model.User;
+import com.bridgelabz.employee_payroll.repository.UserRepository;
+import com.bridgelabz.employee_payroll.utility.JwtUserDetailsService;
+import com.bridgelabz.employee_payroll.utility.JwtUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtility jwtUtility;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+
+    public List<User> getAllEmployees(){
+        return userRepository.findAll();
+    }
+
+    public ResponseEntity<User> getUserById(Long id){
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> deleteUserById(Long id){
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
+        userRepository.delete(user);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+    }
+
+    public ResponseEntity<ResponseDTO> updateUserById(Long id, RegisterDTO request){
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+        ResponseDTO response = new ResponseDTO("User updated successfully", HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+// *******************    register    *******************
+
+    public ResponseEntity<ResponseDTO> registerUser(RegisterDTO request){
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            ResponseDTO response = new ResponseDTO("user already exists", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        User user = new User();
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+
+        ResponseDTO response = new ResponseDTO("User registered successfully", HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+//   ******************* login *******************
+    public ResponseEntity<ResponseDTO> loginUser(@RequestBody LoginDTO request) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        } catch (Exception e) {
+            e.printStackTrace();
+            ResponseDTO response = new ResponseDTO("Invalid Credentials", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        final String token = jwtUtility.generateToken(userDetails.getUsername());
+
+        ResponseDTO response = new ResponseDTO(token,HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
